40ecc24191f9f7b375585f2aee94d9c2f6d75ee1
thorstenalpers <ThorstenAlpers@web.de>
2019-03-31 19:53:57 +0200

    Change app to a SPA with angularjs

--- js/index-loader.js
+++
@@ -1,4 +0,0 @@
-//Load requirejs config, then load the app logic for a page.
-requirejs(['./require-config'], function (common) {
-    requirejs(['index']);
-});
--- js/require-config.js
+++ js/require-config.js
@@ -1,34 +1,35 @@
 requirejs.config({
-    
-/*     baseUrl: '../lib',
- */
+
+    /*     baseUrl: '../lib',
+     */
     /* define libraries to be loaded */
     paths: {
         'angular': '../lib/angular.js/angular',
-        'angular-resourse': '../lib/angular.js/angular-resource.min',
-        'angular-route': '../lib/angular.js/angular-route.min',
+        'ng-resource': '../lib/angular.js/angular-resource.min',
+        'ng-route': '../lib/angular.js/angular-route.min',
         'jquery': '../lib/jquery/jquery.min',
         'particlesJS': '../lib/particles.js/particles.min',
         'bootstrap': '../lib/bootstrap/js/bootstrap.bundle.min',
         'progressbarjs': '../lib/progressbar.js/progressbar.min',
-        'typed': '../lib/typed.js/typed.min'
+        'typed': '../lib/typed.js/typed.min',
+        'app': 'app',
     },
     /* dependencies */
     shim: {
         'angular': {
             deps: ['jquery']
         },
-        'angular-route': {
-            deps: ['angular']}
-        ,
-        'angular-resourse': {
+        'ng-route': {
             deps: ['angular']
-        },     
-        'particlesJS': {
-            deps: ['jquery', 'bootstrap']
         },
-        'index': {
-            deps: ['angular-route', 'jquery', 'bootstrap', 'particlesJS', 'typed']
+        'ng-resource': {
+            deps: ['angular']
+        },
+        'ng-resource': {
+            deps: ['ng-route']
+        },
+        'app': {
+            deps: ['ng-route', 'jquery', 'bootstrap', 'particlesJS', 'typed', 'progressbarjs', 'ng-resource']
         }
     }
 });
