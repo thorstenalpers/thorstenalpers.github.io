c4c2f5998b52909fb6adb3ff0d1f21004c5017d9
netninja <netninja@gmx.de>
2020-05-19 16:14:02 +0200

    deploy react app

---
+++ precache-manifest.0c82db91a86a7bdd101159237409e14b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc866c81c9a9454846bf886934015c53",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6bb00e5638a640189c80",
+    "url": "/static/css/2.66666e14.chunk.css"
+  },
+  {
+    "revision": "283473b8fe6a1e503f4f",
+    "url": "/static/css/main.eda0a711.chunk.css"
+  },
+  {
+    "revision": "6bb00e5638a640189c80",
+    "url": "/static/js/2.1251b56f.chunk.js"
+  },
+  {
+    "revision": "5ce5115c4d177a663a37a299789c2305",
+    "url": "/static/js/2.1251b56f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "283473b8fe6a1e503f4f",
+    "url": "/static/js/main.3279eb80.chunk.js"
+  },
+  {
+    "revision": "1b6c486bcc031584ddcb",
+    "url": "/static/js/runtime-main.3053f720.js"
+  }
+]);
