1484ab161a6e72efbbf901acfa59947d11837e29
netninja <netninja@gmx.de>
2021-05-06 18:44:57 +0200

    cleanup

---
+++ precache-manifest.1d867247cf820c771c81a870b2ff2ad6.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "497c2f6883a814719de6411a1058dc69",
+    "url": "/index.html"
+  },
+  {
+    "revision": "047827bf5bd52f36a481",
+    "url": "/static/css/2.8d4d37cd.chunk.css"
+  },
+  {
+    "revision": "e00bd621186068b1822f",
+    "url": "/static/css/main.f16f1c2c.chunk.css"
+  },
+  {
+    "revision": "047827bf5bd52f36a481",
+    "url": "/static/js/2.a685a4df.chunk.js"
+  },
+  {
+    "revision": "df57eebfaef345460cf185f6e371e75c",
+    "url": "/static/js/2.a685a4df.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e00bd621186068b1822f",
+    "url": "/static/js/main.25e86fa7.chunk.js"
+  },
+  {
+    "revision": "edcffb4e586786910335",
+    "url": "/static/js/runtime-main.b2fe0743.js"
+  },
+  {
+    "revision": "1286a19fc578e6b5a92e6dcb744dec16",
+    "url": "/static/media/dxicons.1286a19f.woff2"
+  },
+  {
+    "revision": "147eb6fe61ff3af12cd8763022ae7f24",
+    "url": "/static/media/dxicons.147eb6fe.ttf"
+  },
+  {
+    "revision": "2b145cefd95f699e2eecc965fb38c8a8",
+    "url": "/static/media/dxicons.2b145cef.woff"
+  }
+]);
